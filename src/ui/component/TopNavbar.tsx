71390180f5a398ddad7798bdb8028162214f5357
icymiss <91042678+icymiss@users.noreply.github.com>
2023-08-24 12:27:32 +0800

    TopNavbar.tsx

--- src/ui/component/TopNavbar.tsx
+++ src/ui/component/TopNavbar.tsx
@@ -11,15 +11,12 @@ import {Link} from "react-router-dom";
 export default function TopNavBar(){
     return <>
 
-
         <Navbar sticky={"top"} bg="dark" variant="dark">
             <Container>
                 <Link to = "/" style={{textDecoration: 'none'}}>
                     <Navbar.Brand>FLORAL SHOP</Navbar.Brand>
                 <Navbar.Toggle/>
                     </Link>
-
-
                 <Nav className="me-auto">
                     <Nav.Link href="/#/">Home</Nav.Link>
                     <Nav.Link href="/#/product/8/">Product</Nav.Link>
